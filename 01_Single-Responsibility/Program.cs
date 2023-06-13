e0d29b136930472ffa88125363c0b84bdf0cde5d
Iasonas <iasonas.ps@gmail.com>
2023-06-13 17:26:52 +0300

    Tried Generic Specification

--- 01_Single-Responsibility/Program.cs
+++ 01_Single-Responsibility/Program.cs
@@ -1,43 +1,92 @@
 ﻿using System;
+using System.Diagnostics;
 //using System.Security.Cryptography.X509Certificates;
 using static System.Console;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SingleResponsibilityPrinciple
 {
     public class Journal
     {
         private readonly List<string> entries = new List<string>();
-        private static int count = 0; 
+        private static int count = 0;
 
         public int AddEntry(string text)
         {
             entries.Add($"{++count} : {text}");
             return count;
         }
-        public void RemoveEntry(int  index) 
+        public void RemoveEntry(int index)
         {
             entries.RemoveAt(index);
         }
-        public override string ToString() 
+        public override string ToString()
         {
             //return string.Join(", ", entries.ToArray());
             return string.Join(Environment.NewLine, entries);
         }
+        #region Violates the 'Single Responsibility' Principle.We will make another Class instead
+        //public void Save(string filename)
+        //{
+        //    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        //    string filePath = Path.Combine(desktopPath, filename);
+        //    WriteLine(filePath);
+        //    File.WriteAllText(filePath, this.ToString());
+        //}
+        //public static Journal Load(string filename)
+        //{
+        //    return null;
+        //}
+        //public void Load(Uri uri)
+        //{
 
-        public void Save(string filename)
-        {
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string filePath = Path.Combine(desktopPath, filename);
-            WriteLine(filePath);
-            File.WriteAllText(filePath, this.ToString());
-        }
-
+        //} 
+        #endregion 
     }
 
+    public class Persistence
+    {
+        //Save File To Desktop...
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = Path.Combine(desktopPath, filename);
+                File.WriteAllText(filePath, journal.ToString());
+            }
+
+        }
+    }
     internal class Program
     {
+        public static void SearchItOnGoogle(string text)
+        {
+            var openEdge = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                Arguments = "http://google.com/search?q=" + text,
+                WindowStyle = ProcessWindowStyle.Minimized,
+                CreateNoWindow = false,
+            };
+            var x = Process.Start(openEdge);
+
+        }
         static void Main(string[] args)
         {
+
+            var os = Environment.OSVersion.ToString();
+            WriteLine(os );
+
+            #region Changing Working Directory
+            //string newWorkingDirectory = @"C:\";
+            //Directory.SetCurrentDirectory(newWorkingDirectory);
+            //WriteLine($"This is the new Working Directory : '{Directory.GetCurrentDirectory()}'");
+
+            #endregion
+            SearchItOnGoogle("that+is+a+search");
+
             WriteLine("Journal :");
             var j = new Journal();
 
@@ -45,13 +94,21 @@ namespace SingleResponsibilityPrinciple
             j.AddEntry("Beatiful fucking entry");
             j.AddEntry(" not so innovative entry0");
             j.AddEntry(" not so innovative entry1");
-            j.AddEntry(" not so innovative entry2"); 
+            j.AddEntry(" not so innovative entry2");
             j.AddEntry(" not so innovative entry3");
             j.AddEntry(" not so innovative entry4");
-            
+            j.AddEntry(" not so innovative entry5");
+            j.AddEntry("entry at 13/06/23");
+
+            WriteLine("--------------------------------");
+            Persistence persistence = new Persistence();
+            persistence.SaveToFile(j,"Journal.txt",false);
             WriteLine(j);
-            j.Save("test.txt");
-            ReadLine();
+
+            ReadKey();
+
         }
+
+
     }
 }
--- 02_Open-Closed/Program.cs
+++ 02_Open-Closed/Program.cs
@@ -1,10 +1,176 @@
-﻿namespace _02_OpenClosedPrinciple
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+
+namespace _02_OpenClosedPrinciple
 {
+    #region Basic Entities
+    public enum Color 
+    {
+        red, green, blue, white, black
+    }
+    public enum Size
+    {
+        small, medium, large, huge
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+        public override string ToString()
+        {
+            return $" {Color}------>{Size}------>{Name}";
+        }
+    }
+    #endregion
+    public class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+        public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color)
+                {
+                    yield return product;
+                }
+            }
+        }
+        public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color && product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+    }
+    //Proper way of adding functionality is via using Interfaces
+    #region Interfaces
+    public interface ISpecification<T>
+    {
+       bool IsSatisfied(T t);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items,ISpecification<T> spec);
+    }
+    #endregion
+    public class ColorSpec :ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpec(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+    public class SizeSpec : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpec(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size.Equals(size);
+        }
+    }
+
+    #region --->GenericSpecification Challenge!!!<---
+    public class GenericSpecification<T> : ISpecification<Product> 
+    //{
+    //    private string Property;
+
+    //    public GenericSpecification(T s )
+    //    {
+    //        Property = s.GetType().Name;
+    //    }
+    //    public bool IsSatisfied(Product t)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+    #endregion
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach(Product product in items)
+            {
+                if(spec.IsSatisfied(product))
+                {
+                    yield return product;
+                }
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Products
+            var apple = new Product("apple", Color.red, Size.small);
+            var tree = new Product("Tree", Color.red, Size.large);
+            var house = new Product("House", Color.blue, Size.large);
+            var camperVan = new Product("CamperVan", Color.blue, Size.small); 
+            #endregion
+            Product[] products = { apple, tree, house, camperVan };
+            WriteLine("Violating the Open-Closed Principle");
+            #region
+            WriteLine("-----------------------------------");
+            WriteLine("Large products :");
+            var outcome00 = ProductFilter.FilterBySize(products, Size.large);
+            outcome00.ToList().ForEach(product => { WriteLine(product); });
+            WriteLine("Red products :");
+            var outcome01 = ProductFilter.FilterByColor(products, Color.red);
+            outcome01.ToList().ForEach(product => { WriteLine(product); });
+            WriteLine("Red and large products :");
+            var outcome02 = ProductFilter.FilterBySizeAndColor(products, Size.large, Color.red);
+            outcome02.ToList().ForEach(product => { WriteLine(product); });
+            #endregion
+            WriteLine("=================================================================");
+            WriteLine("Following the Open-Closed Principle");
+            #region
+            WriteLine("-----------------------------------");
+            WriteLine("Blue products : ");
+            BetterFilter bF = new BetterFilter();
+            ColorSpec redSpec = new ColorSpec(Color.red);
+            SizeSpec largeSpec = new SizeSpec(Size.large);
+            var outcome03 = bF.Filter(products, redSpec);
+            outcome03.ToList().ForEach(product => { WriteLine(product);});
+            WriteLine("Large Products :");
+            var outcome04 = bF.Filter(products, largeSpec);
+            outcome04.ToList().ForEach(product => {WriteLine(product);});
+            #endregion
         }
     }
 }
