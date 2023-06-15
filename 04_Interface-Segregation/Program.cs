b232ed22b7359654fd93d541921a7ac4741abac5
Iasonas <iasonas.ps@gmail.com>
2023-06-15 12:55:35 +0300

    Dependency_Inversion

--- 04_Interface-Segregation/Program.cs
+++ 04_Interface-Segregation/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
 
     }
     #region Violating I-S Principle
+    //A huge interface "To Rule them All"....
+    //Thats not practical at all
     public interface IMachine
     {
         void Print(Document d);
@@ -31,11 +33,13 @@ namespace ConsoleApp1
     }
 
     //The oldFashionedPrinter only Prints!
+    //But we only have one interface for all functionalities...
+    //so we have to implement 2 extra methods, that in this exact class are completely redundant...
     public class OldFashionedPrinter : IMachine
     {
         public void Print(Document d)
         {
-           //
+           //implementation of print method
         }
         public void Fax(Document d)
         {
@@ -50,7 +54,7 @@ namespace ConsoleApp1
 
     #region Following the I-S Principle
     //The main difference is that we create an interface
-    //for each different function we may want to implement in our classes
+    //for each different function, we may want to implement in our classes.
     public interface IPrinter
     {
         void Print(Document d);
@@ -76,29 +80,65 @@ namespace ConsoleApp1
            //
         }
     }
-    //Higher level interface 
+    //or Higher level interface inherits many "lower level" interfaces
     public interface IMultiMachine : IPrinter,IScanner,IFax
     {
 
     }
     public class MultiDevice : IMultiMachine
     {
+        private IPrinter printer;
+        private IScanner scanner;
+        private IFax fax;
+
+        public MultiDevice(IPrinter printer, IScanner scanner, IFax fax)
+        {
+            if(printer == null)     
+            {
+                throw new ArgumentNullException(paramName : nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            if (fax == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(fax));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+            this.fax = fax;
+        }
+
         public void Fax(Document d)
         {
-           //
+            fax.Fax(d);
         }
+
         public void Print(Document d)
         {
-           //
+            printer.Print(d);
         }
+
         public void Scan(Document d)
         {
-            //
+            scanner.Scan(d);
         }
+
+        //public void Scan(Document d)
+        //{
+        //    //
+        //}
+        //public void Print(Document d)
+        //{
+        //   //
+        //}
+        //public void Fax(Document d)
+        //{
+        //   //
+        //}
     }
     #endregion
-
-
 
     internal class Program
     {
--- 05_Dependency-Inversion/Program.cs
+++ 05_Dependency-Inversion/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace _05_Dependency_Inversion
+﻿using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+namespace _05_Dependency_Inversion
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public enum Relationship
         {
-            Console.WriteLine("Hello, World!");
+            Parent, Child, Sibling
+        }
+        public class Person
+        {
+            public string Name;
+
+        }
+        //low level
+        public class Relationships
+        {
+            //Why isnt this List static?????
+            private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+            public void AddParentAndChild(Person parent, Person child)
+            {
+                relations.Add((parent, Relationship.Parent, child));
+                relations.Add((child, Relationship.Child, parent));
+
+                WriteLine(parent.Name+"  is parent of "+child.Name);
+            }
+
+            public List<(Person, Relationship, Person)> Relations => relations;
+        }
+        public class Research
+        {
+            public Research(Relationships relationships)
+            {
+                var relations = relationships.Relations;
+                //relations.ForEach(x =>   WriteLine(x));
+                foreach (var rel in relations.Where(x => x.Item1.Name == "Jack" && x.Item2 == Relationship.Parent) )
+                {
+                    WriteLine($"{rel.Item1.Name} has a child called {rel.Item3.Name}");
+                }
+            }
+            static void Main(string[] args)
+            {
+                var parent = new Person { Name = "Jack" };
+                var child0 = new Person { Name = "Anna" };
+                var child1 = new Person { Name = "Stefanos" };
+                var grand_Child = new Person { Name = "nikos" };
+
+                var relationships = new Relationships();
+                relationships.AddParentAndChild(parent, child0);
+                relationships.AddParentAndChild(parent, child1);
+                relationships.AddParentAndChild(child0, grand_Child);
+                WriteLine("---------------------------");
+                 new Research(relationships);
+
+            }
         }
     }
 }
